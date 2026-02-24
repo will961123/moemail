7f99455d8b0f69d0b1d32697603a56bcc789259d
wupf <wupf@bjhzwq.com>
2026-02-24 17:36:06 +0800

    feat: 网站管理新增用户注册方式开关

--- app/components/profile/user-details-dialog.tsx
+++ app/components/profile/user-details-dialog.tsx
@@ -28,7 +28,9 @@ interface User {
   email: string | null
   username: string | null
   emailCount: number
-  messageCount: number
+  receivedCount: number
+  sentCount: number
+  createdAt: string
   roles: Array<{
     id: string
     name: string
